a9c8a718b4e4975bf6459a965b86ebb20e89d925
RomuloGarciaSales <garciasalesrea@gmail.com>
2025-05-20 17:27:38 -0600

    Lab 8 Pensamiento Computacional

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+Console.WriteLine("Ejercicio 1");
+Console.WriteLine("Bienvenido, ingrese su nombre");
+string Nombre;
+Nombre= Console.ReadLine();
+Console.WriteLine("Ingrese su altura con un número entero y dos decimales");
+double Altura;
+Altura=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingrese su edad");
+int Edad= Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Ingrese la inicial de su mascota");
+char Inicialmascota;
+Inicialmascota= Convert.ToChar(Console.ReadLine());
+Console.WriteLine(Nombre);
+Console.WriteLine(Altura);
+Console.WriteLine(Edad);
+Console.WriteLine(Inicialmascota);
