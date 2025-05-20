f1a8e7256de179828bc5755926dcd6e34d7e8ed0
RomuloGarciaSales <garciasalesrea@gmail.com>
2025-05-20 17:32:24 -0600

    Lab 14 Pensamiento Computacional

--- Program.cs
+++ Program.cs
@@ -1,77 +1,14 @@
-﻿Console.WriteLine("PROGRAMA PARA CONVERSIÓN DE TEMPERATURA");
-int opcion;
-    do
-    {
-        MostrarMenu();
-         Console.Write("Ingresar opción solicitada:");
+﻿using System.Diagnostics.CodeAnalysis;
 
-        if (int.TryParse(Console.ReadLine(), out opcion))
-        {
-            switch (opcion)
-            {
-                case 1:
-                    CelciusAFahrenheit();
-                    break;
-                case 2:
-                    FahrenheitACelcius();
-                    break;
-                case 3:
-                    MostrarInformacionProgramador();
-                    break;
-                case 4:
-                    Console.WriteLine("El programa se cerró");
-                    break;
-                default:
-                    Console.WriteLine("Seleccione una opción valida.");
-                    break;
-            }
-        }
-        else
-        {
-            Console.WriteLine("Entrada inválida. Por favor, ingrese un número del menú.");
-        }
-    } while (opcion != 4);
+Console.WriteLine("Programa para notas de los Estudiantes");
+string [] nombres = ["Juan","Pedro","Luisa","Adriana","Sofia"];
+int [] notas = [88,75,96,77,59];
+int suma = 0;
 
-static void MostrarMenu()
+for (int a=0; a<notas.Length; a++)
 {
-    Console.WriteLine("Opciones a elegir en el programa:");
-    Console.WriteLine("1. Convertir de Celsius a Fahrenheit");
-    Console.WriteLine("2. Convertir de Fahrenheit a Celsius");
-    Console.WriteLine("3. Información del programador");
-    Console.WriteLine("4. Salir");
+    Console.WriteLine($"Las notas correspondientes son {a}:" + nombres[a]+  "-" + notas[a]);
 }
 
-static void CelciusAFahrenheit()
-{
-    Console.WriteLine("Ingrese la cantidad de grados en Celsius:");
-    if (double.TryParse(Console.ReadLine(), out double celcius))
-    {
-        double fahrenheit = (celcius * 9 / 5) + 32;
-        Console.WriteLine($"{celcius}°C equivalen a {fahrenheit}°F");
-    }
-    else
-    {
-        Console.WriteLine("Entrada inválida. Por favor, ingrese un valor numérico.");
-    }
-}
-
-static void FahrenheitACelcius()
-{
-    Console.WriteLine("Ingrese la cantidad de grados en Fahrenheit:");
-    if (double.TryParse(Console.ReadLine(), out double fahrenheit))
-    {
-        double celcius = (fahrenheit - 32) * 5 / 9;
-        Console.WriteLine($"{fahrenheit}°F equivalen a {celcius}°C");
-    }
-    else
-    {
-        Console.WriteLine("Entrada inválida. Por favor, ingrese un valor numérico.");
-    }
-}
-
-static void MostrarInformacionProgramador()
-{
-    Console.WriteLine("Información del Programador:");
-    Console.WriteLine("Nombre: Rómulo García.");
-    Console.WriteLine("Carrera: Ingeniería en Informática y Sistemas.");
-}
+int promedio = suma/notas.Length;
+Console.WriteLine($"El promedio de las notas es:" + promedio);
